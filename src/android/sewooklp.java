b4f20d6fc7cf1369f16a23b4a916b373e01e413f
Juan Carlos Flores <jcflores_martineznajam@hotmail.com>
2020-07-03 20:41:54 -0600

    Import all major enums

--- src/android/sewooklp.java
+++ src/android/sewooklp.java
@@ -13,7 +13,7 @@ import org.json.JSONObject;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothClass.Device;
+import android.bluetooth.BluetoothClass.Device.Major.*;
 import android.content.Intent;
 import android.content.IntentFilter;
 
